81311eb32807ae2270ca3568aa7d3c2631111c36
Michał Hewelt <mhewelt9@gmail.com>
2022-11-19 21:21:28 +0100

    combine encrypt and decrypt functions

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-def encrypt(plain_text, shift_amount):
-    encrypted_text = ""
-    for letter in plain_text:
+def caesar(user_text, shift_amount, direction_arg):
+    if direction_arg == "decode":
+        shift_amount *= -1
+    caesar_text = ""
+    for letter in user_text:
         pos = alphabet.index(letter)
         new_letter = alphabet[pos + shift_amount]
-        encrypted_text += new_letter
-    print(f"Here's the encoded result: {encrypted_text}")
+        caesar_text += new_letter
+    print(f"Here's the {direction_arg}d result: {caesar_text}")
 
 
-def decrypt(encrypted_text, shift_amount):
-    decrypted_text = ""
-    for letter in encrypted_text:
-        pos = alphabet.index(letter)
-        new_letter = alphabet[pos - shift_amount]
-        decrypted_text += new_letter
-    print(f"Here's the decoded result: {decrypted_text}")
-
-
-# find different solution than doubling alphabet
+# find different solution than doubling alphabet to index out of range
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -25,7 +18,4 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-if direction == "encode":
-    encrypt(text, shift)
-else:
-    decrypt(text, shift)
+caesar(text, shift, direction)
