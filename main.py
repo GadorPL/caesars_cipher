eb13a0466b073d9074833561dc5a501651576203
Michał Hewelt <mhewelt9@gmail.com>
2022-11-19 22:18:26 +0100

    improve user experience

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-def caesar(user_text, shift_amount, direction_arg):
-    if direction_arg == "decode":
+from art import logo
+
+
+def caesar(user_text, shift_amount, cipher_direction):
+    if cipher_direction == "decode":
         shift_amount *= -1
     caesar_text = ""
-    for letter in user_text:
-        pos = alphabet.index(letter)
-        new_letter = alphabet[pos + shift_amount]
-        caesar_text += new_letter
-    print(f"Here's the {direction_arg}d result: {caesar_text}")
+    for char in user_text:
+        if char in alphabet:
+            pos = alphabet.index(char)
+            new_letter = alphabet[pos + shift_amount]
+            caesar_text += new_letter
+        else:
+            caesar_text += char
+    print(f"Here's the {cipher_direction}d result: {caesar_text}")
 
 
 # find different solution than doubling alphabet to index out of range
@@ -14,8 +20,15 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-caesar(text, shift, direction)
+keep_going = True
+while keep_going:
+    print(logo)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if shift > 26:  # 26 letters in alphabet
+        shift %= 26  # shift = shift % 26
+    caesar(text, shift, direction)
+    want_continue = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if want_continue == "no":
+        keep_going = False
